95969055212ff23138667e0ce2b411724bf11710
Tyler Moline <tmoline@devpipeline.com>
2023-06-12 13:39:47 -0600

    added on click functionality

--- js/index.js
+++ js/index.js
@@ -1,18 +1,6 @@
-// Due: Monday 6/12/2023
-// Project 2: Virtual ATM Machine (OOP Project)
-// -Build a console app that mimics an atm Machine
-// -Persistent balance ( make it adaptable [add remove, null, get])
-// -Main Menu that routes to other menus
-// **********
-// WELCOME
-// view balance etc..
-// ***********
-// -redirect back to the main menu
-// -Error handling for withdrawals
-// -Deposit
-// -Exit the program via selection
-// -Receipt (edited)
+atmStart.addEventListener("click", atm);
 
+// This is creating the user classes
 class bankUser {
   constructor(userName, pin) {
     this.userName = userName;
@@ -37,69 +25,79 @@ class bankUser {
 usersArr = [];
 
 const tyler = new bankUser("tyler", 1234);
-const ian = new bankUser("Ian", 1234);
-const james = new bankUser("James", 1234);
+const ian = new bankUser("ian", 1234);
+const james = new bankUser("james", 1234);
 
+// This is creating instances of the object
 usersArr.push(tyler);
 usersArr.push(ian);
 usersArr.push(james);
 
-let userInputName = prompt("Please input your user name");
-let userInputPin = Number(prompt("Please input your pin"));
-let currentUser;
+function atm() {
+  // This is getting the username and password to log in and making sure its correct
+  let userInputName = prompt("Please input your user name").toLowerCase();
+  let userInputPin = Number(prompt("Please input your pin"));
+  let currentUser;
 
-for (const i in usersArr) {
-  if (
-    usersArr[i].userName === userInputName &&
-    usersArr[i].pin === userInputPin
-  ) {
-    currentUser = usersArr[i];
-    //console.log(currentUser);
+  for (const i in usersArr) {
+    if (
+      usersArr[i].userName === userInputName &&
+      usersArr[i].pin === userInputPin
+    ) {
+      currentUser = usersArr[i];
+    }
   }
-}
 
-console.log(
-  "Welcome " +
-    currentUser.userName +
-    " to the bank of Devpipeline!\n------------------------------------\nPlease select from the options below:"
-);
+  // Prints the welcome statement and lets the user pick an option
+  console.log(
+    "Welcome " +
+      currentUser.userName +
+      " to the bank of Devpipeline!\n------------------------------------\nPlease select from the options below:"
+  );
 
-let options = ["Deposit", "Withdraw", "Print receipt", "Quit"];
+  let options = ["Deposit", "Withdraw", "Print receipt", "Quit"];
 
-for (let i = 0; i < options.length; i++) {
-  console.log(i + 1, options[i]);
-}
+  for (let i = 0; i < options.length; i++) {
+    console.log(i + 1, options[i]);
+  }
 
-let userChoice;
-let quitOption = false;
+  let userChoice;
+  let quitOption = false;
 
-while (quitOption === false) {
-  switch (
-    (userChoice = Number(
-      prompt("Please choose a NUMBER from the list of options")
-    ))
-  ) {
-    case 1:
-      let depositAmount = Number(prompt("Please input your deposit amount"));
-      currentUser.deposit(depositAmount);
-      console.log(currentUser.balance);
-      break;
-    case 2:
-      let withdrawAmount = Number(prompt("Please input your withdraw amount"));
-      if (withdrawAmount > currentUser.balance) {
-        console.log("Insufficient balance");
-      } else {
-        currentUser.withdraw(withdrawAmount);
-        console.log(currentUser.balance);
-      }
-      break;
-    case 3:
-      console.log("Your current balance is " + currentUser.balance);
-      break;
+  // Depending on the user input will determine what option they can use and this executes is
+  while (quitOption === false) {
+    switch (
+      (userChoice = Number(
+        prompt("Please choose a NUMBER from the list of options")
+      ))
+    ) {
+      case 1:
+        let depositAmount = Number(prompt("Please input your deposit amount"));
 
-    case 4:
-      console.log("Thanks for using our bank!");
-      quitOption = true;
-      break;
+        currentUser.deposit(depositAmount);
+        console.log("$" + currentUser.balance + " is your current balance");
+
+        break;
+      case 2:
+        console.log("$" + currentUser.balance + " is your current balance");
+        let withdrawAmount = Number(
+          prompt("Please input your withdraw amount")
+        );
+        if (withdrawAmount > currentUser.balance) {
+          console.log("Insufficient balance");
+        } else {
+          currentUser.withdraw(withdrawAmount);
+          console.log(currentUser.balance);
+        }
+        break;
+      case 3:
+        console.log("Your current balance is " + currentUser.balance);
+        break;
+
+      case 4:
+        console.log("Thanks for using our bank!");
+        quitOption = true;
+        break;
+    }
   }
 }
